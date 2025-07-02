b54ec000ff1b27b0c8d8b686d2a3c928841a9e9a
satishnadipalli <satishnadipalli1@gmail.com>
2025-07-02 13:30:41 +0530

    resolved the build errrors for production deployement

--- next.config.ts
+++ next.config.ts
@@ -3,23 +3,17 @@ const nextConfig = {
   output: "standalone",
   basePath: "/hms",
   experimental: {
-    serverActions: true
+    serverActions: true,
   },
-
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has type errors.
-    ignoreDuringBuilds: true,
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
