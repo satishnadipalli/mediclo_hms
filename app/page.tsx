746bb8f593ed35ef3a98b66ab3aa7a7dfe768f02
satishnadipalli <satishnadipalli1@gmail.com>
2025-07-02 15:43:02 +0530

    updated all the deeplinks routes to use the /hms as base path

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace("/login"); 
+    router.replace("/hms/login"); 
   }, [router]);
 
   return null;
