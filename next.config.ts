1bd0921ceb610d7da90b21260ed901ee3d544c15
satishnadipalli <satishnadipalli1@gmail.com>
2025-07-02 12:10:40 +0530

    resolved the build errrors for production deployement

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   basePath: "/hms",
+  experimental: {
+    serverActions: true
+  }
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
